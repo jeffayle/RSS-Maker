f3380102c51703b114f0d30d887f169a0ba42d25
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-24 16:12:56 -0500

    Function to create a new feed

--- lib.php
+++ lib.php
@@ -6,4 +6,24 @@ function db_connect() {
     $db = new SQLiteDatabase('data/database.sqlite');
     return $db;
 }
+
+/* Create a new feed */
+function new_feed($db, $name, $desc, $url, $cat, $copyright, $image_url,
+        $image_title, $lang, $webmaster, $editor) {
+     $name = sqlite_escape_string($name);
+     $desc = sqlite_escape_string($desc);
+     $url = sqlite_escape_string($url);
+     $cat = sqlite_escape_string($cat);
+     $copyright = sqlite_escape_string($copyright);
+     $image_url = sqlite_escape_string($image_url);
+     $image_title = sqlite_escape_string($image_title);
+     $lang = sqlite_escape_string($lang);
+     $webmaster = sqlite_escape_string($webmaster);
+     $editor = sqlite_escape_string($editor);
+     $db->query("INSERT INTO feeds (title,description,link,category,copyright,
+                image_url,image_title,language,webmaster,editor)
+            VALUES ('$name','$desc','$url','$cat','$copyright','$image_url',
+                '$image_title','$lang','$webmaster','$editor')");
+     return true;
+}
 ?>
