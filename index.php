c007890591d0e8675f3f844edf36efbc7b4b05ce
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-24 15:59:02 -0500

    New feed form

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <? require('header.php'); ?>
 
-Hello, World!
+<h2>Feeds</h2>
+<!-- TODO -->
+
+<h2>New Feed</h2>
+<form action="newfeed.php" method="post">
+    Name: <input type="text" name="name" /> <br />
+    Description: <input type="text" name="desc" /> <br />
+    URL: <input type="text" name="url" /> <br />
+    Category: <input type="text" name="cat" /> <br />
+    Copyright: <input type="text" name="copyright" /> <br />
+    Image URL: <input type="text" name="image_url" /> <br />
+    Image Title: <input type="text" name="image_title" /> <br />
+    Language: <input type="text" name="lang" /> <br />
+    Webmaster's Email: <input type="text" name="webmaster" /> <br />
+    Editor's Email: <input type="text" name="editor" /> <br />
+    <input type="submit" value="Create feed" />
+</form>
 
 <? require('footer.php'); ?>
