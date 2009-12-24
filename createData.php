4edce836fc30e7669b5fa5d2548ef75e9c11944f
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-24 15:49:17 -0500

    Create feed table

---
+++ createData.php
@@ -0,0 +1,22 @@
+<?php
+    /* Create the SQL tables, delete this after it has been run */
+    require('lib.php');
+    $db = db_connect();
+
+    /* Feeds table */
+    $db->query('CREATE TABLE feeds (
+        id INTEGER PRIMARY KEY,
+        title STRING,
+        description STRING,
+        link STRING,
+        category STRING,
+        copyright STRING,
+        image_url STRING,
+        image_title STRING,
+        language VARCHAR(5),
+        webmaster STRING,
+        editor STRING
+    )');
+?>
+
+Database created.
