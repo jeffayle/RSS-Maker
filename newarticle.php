582a201f70ecd5cf843955abcac5fc1f4a2c4aa6
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-24 21:45:55 -0500

    newarticle.php can now update existing article

--- newarticle.php
+++ newarticle.php
@@ -1,8 +1,12 @@
 <?php
     require('lib.php');
     $db = db_connect();
-
-    $feed = $_POST['feed'];
+    $update = ($_SERVER['QUERY_STRING']=='edit') ? $_POST['article'] : 0;
+    
+    if ($update == 0)
+        $feed = $_POST['feed'];
+    else
+        $feed = 0; //Does not know feed if updating, does not change it.
     $title = $_POST['title'];
     $desc = $_POST['desc'];
     $link = $_POST['link'];
@@ -10,7 +14,7 @@
     $cat = $_POST['cat'];
     $pubDate = $_POST['pubDate']*1;
 
-    new_article($db, $feed,$title,$desc,$link,$auth,$cat,$pubDate);
+    new_article($db, $feed,$title,$desc,$link,$auth,$cat,$pubDate,$update);
 
     header("Location: feed.php?f=$feed");
 ?>
